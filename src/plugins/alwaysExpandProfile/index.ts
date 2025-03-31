6c5b12550c3a668da7bcac3ebdf6c0bb7965d8fb
MutanPlex <barisal54@gmail.com>
2025-03-31 17:30:21 +0300

    AlwaysExpandProfile: Plugin added

---
+++ src/plugins/alwaysExpandProfile/index.ts
@@ -0,0 +1,36 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { PcDevs } from "@utils/constants";
+import { openUserProfile } from "@utils/discord";
+import definePlugin from "@utils/types";
+import { User } from "discord-types/general";
+
+export default definePlugin({
+    name: "AlwaysExpandProfile",
+    description: "Always display a user's full popout",
+    authors: [PcDevs.MutanPlex, PcDevs.thororen],
+    patches: [
+        {
+            find: '"UserProfilePopout"',
+            replacement: {
+                match: /(?<=user:(\i).*?"PRESS_VIEW_PROFILE".{0,50})return/,
+                replace: "return $self.openUserProfile($1);",
+            },
+        },
+        {
+            find: '"BotUserProfilePopout"',
+            replacement: {
+                match: /(?<=user:(\i).*?"PRESS_VIEW_PROFILE".{0,50})return/,
+                replace: "return $self.openUserProfile($1);",
+            },
+        },
+    ],
+    openUserProfile(user: User) {
+        openUserProfile(user.id);
+    },
+});
