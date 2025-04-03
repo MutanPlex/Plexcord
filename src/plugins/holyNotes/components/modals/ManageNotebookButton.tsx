0475557a1ef5771b59fd80003647a5f4db452129
MutanPlex <barisal54@gmail.com>
2025-04-03 21:51:36 +0300

    HolyNotes: Plugin Added

---
+++ src/plugins/holyNotes/components/modals/ManageNotebookButton.tsx
@@ -0,0 +1,29 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { openModal } from "@utils/modal";
+import { Button, React } from "@webpack/common";
+
+import NotebookCreateModal from "./NotebookCreateModal";
+import NotebookDeleteModal from "./NotebookDeleteModal";
+
+export default ({ notebook, setNotebook }: { notebook: string, setNotebook: React.Dispatch<React.SetStateAction<string>>; }) => {
+    const isNotMain = notebook !== "Main";
+
+    return (
+        <Button
+            color={isNotMain ? Button.Colors.RED : Button.Colors.GREEN}
+            onClick={
+                isNotMain
+                    ? () => openModal(props => <NotebookDeleteModal {...props} notebook={notebook} onChangeTab={setNotebook} />)
+                    : () => openModal(props => <NotebookCreateModal {...props} />)
+            }
+        >
+            {isNotMain ? "Delete Notebook" : "Create Notebook"}
+        </Button>
+    );
+};
---
+++ src/plugins/holyNotes/components/modals/NotebookCreateModal.tsx
@@ -0,0 +1,41 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize } from "@utils/modal";
+import { Button, React, Text, TextInput } from "@webpack/common";
+
+import noteHandler from "../../NoteHandler";
+
+export default (props: ModalProps & { onClose: () => void; }) => {
+    const [notebookName, setNotebookName] = React.useState("");
+
+    const handleCreateNotebook = React.useCallback(() => {
+        if (notebookName !== "") noteHandler.newNoteBook(notebookName);
+        props.onClose();
+    }, [notebookName]);
+
+    return (
+        <div>
+            <ModalRoot className="pc-create-notebook" size={ModalSize.SMALL} {...props}>
+                <ModalHeader className="pc-notebook-header">
+                    <Text tag="h3">Create Notebook</Text>
+                    <ModalCloseButton onClick={props.onClose} />
+                </ModalHeader>
+                <ModalContent>
+                    <TextInput
+                        value={notebookName}
+                        placeholder="Notebook Name"
+                        onChange={value => setNotebookName(value)}
+                        style={{ marginBottom: "10px" }} />
+                </ModalContent>
+                <ModalFooter>
+                    <Button onClick={handleCreateNotebook} color={Button.Colors.GREEN}>Create Notebook</Button>
+                </ModalFooter>
+            </ModalRoot>
+        </div>
+    );
+};
