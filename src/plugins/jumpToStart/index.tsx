4d71d354b8443e185ecb94c01a3c985b2469d008
MutanPlex <barisal54@gmail.com>
2025-04-05 21:21:44 +0300

    JumpToStart: Plugin Added

--- src/plugins/jumpToStart/index.tsx
+++ src/plugins/jumpToStart/index.tsx
@@ -1,6 +1,7 @@
 /*
- * Vencord, a Discord client mod
+ * Plexcord, a modification for Discord's desktop app
  * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
