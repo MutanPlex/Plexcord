f20616028c2a5aaa18bf4cb1e2c1004477169512
MutanPlex <barisal54@gmail.com>
2025-04-06 23:19:19 +0300

    New dev added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -749,6 +749,10 @@ export const PcDevs = /* #__PURE__*/ Object.freeze({
         name: "Hen",
         id: 279266228151779329n
     },
+    vappstar: {
+        name: "vappstar",
+        id: 747192967311261748n
+    },
 } satisfies Record<string, PcDev>);
 
 export const PcDevsById = /* #__PURE__*/ (() =>
