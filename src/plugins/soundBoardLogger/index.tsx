c53d58fa0c72b6bb23eceb608d1eef964d6017df
MutanPlex <barisal54@gmail.com>
2025-04-08 04:20:51 +0300

    SoundBoardLogger: Plugin added

---
+++ src/plugins/soundBoardLogger/index.tsx
@@ -0,0 +1,66 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { addChatBarButton, removeChatBarButton } from "@api/ChatButtons";
+import { disableStyle, enableStyle } from "@api/Styles";
+import ErrorBoundary from "@components/ErrorBoundary";
+import { Devs, PcDevs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { FluxDispatcher } from "@webpack/common";
+
+import { ChatBarIcon } from "./components/Icons";
+import { OpenSBLogsButton } from "./components/SoundBoardLog";
+import settings from "./settings";
+import { updateLoggedSounds } from "./store";
+import styles from "./styles.css?managed";
+import { getListeners } from "./utils";
+
+export default definePlugin({
+    name: "SoundBoardLogger",
+    authors: [Devs.Moxxie, PcDevs.Fres, Devs.echo, PcDevs.thororen, PcDevs.MutanPlex],
+    description: "Logs all soundboards that are played in a voice chat and allows you to download them",
+    dependencies: ["ChatInputButtonAPI"],
+    patches: [
+        {
+            predicate: () => settings.store.IconLocation === "toolbar",
+            find: "toolbar:function",
+            replacement: {
+                match: /(function \i\(\i\){)(.{1,500}toolbar.{1,500}mobileToolbar)/,
+                replace: "$1$self.addSBIconToToolBar(arguments[0]);$2"
+            }
+        }
+    ],
+    settings,
+    addSBIconToToolBar(e: { toolbar: React.ReactNode[] | React.ReactNode; }) {
+        if (Array.isArray(e.toolbar))
+            return e.toolbar.push(
+                <ErrorBoundary noop={true}>
+                    <OpenSBLogsButton />
+                </ErrorBoundary>
+            );
+
+        e.toolbar = [
+            <ErrorBoundary noop={true} key={"SoundBoardLogger"} >
+                <OpenSBLogsButton />
+            </ErrorBoundary>,
+            e.toolbar,
+        ];
+    },
+    start() {
+        enableStyle(styles);
+        FluxDispatcher.subscribe("VOICE_CHANNEL_EFFECT_SEND", async sound => {
+            if (!sound?.soundId) return;
+            await updateLoggedSounds(sound);
+            getListeners().forEach(cb => cb());
+        });
+        if (settings.store.IconLocation === "chat") addChatBarButton("pc-soundlog-button", ChatBarIcon);
+    },
+    stop() {
+        disableStyle(styles);
+        if (settings.store.IconLocation === "chat") removeChatBarButton("pc-soundlog-button");
+    }
+});
