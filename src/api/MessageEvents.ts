9595563321a68e1ad3ac4505f5f3386991fb9a28
MutanPlex <barisal54@gmail.com>
2025-07-10 09:09:12 +0300

    MessageReplyOptions updated

--- src/api/MessageEvents.ts
+++ src/api/MessageEvents.ts
@@ -62,6 +62,8 @@ export interface MessageReplyOptions {
     messageReference: Message["messageReference"];
     allowedMentions?: {
         parse: Array<string>;
+        users?: Array<string>;
+        roles?: Array<string>;
         repliedUser: boolean;
     };
 }
