d8cbd532065b33c7e8f682472d78625e16a18b19
MutanPlex <barisal54@gmail.com>
2025-07-10 09:15:08 +0300

    Add PassiveUpdateState interface for managing passive update states in the application

---
+++ src/webpack/common/types/passiveupdatestate.d.ts
@@ -0,0 +1,42 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { VoiceState } from "./voicestate";
+
+export interface PassiveUpdateState {
+    type: string;
+    guildId: string;
+    members?: ({
+        user: {
+            avatar: null | string;
+            communication_disabled_until: null | string;
+            deaf: boolean;
+            flags: number;
+            joined_at: string;
+            mute: boolean;
+            nick: string;
+            pending: boolean;
+            premium_since: null | string;
+        };
+        roles: (string)[];
+        premium_since: null | string;
+        pending: boolean;
+        nick: string | null;
+        mute: boolean;
+        joined_at: string;
+        flags: number;
+        deaf: boolean;
+        communication_disabled_until: null | string;
+        avatar: null | string;
+    })[];
+    channels: ({
+        lastPinTimestamp?: string;
+        lastMessageId: string;
+        id: string;
+    })[];
+    voiceStates?: VoiceState[];
+}
