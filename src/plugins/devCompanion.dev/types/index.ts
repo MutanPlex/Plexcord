1ae07cf8a004048be88d3640a4357c8bdbfee1fe
MutanPlex <barisal54@gmail.com>
2025-07-12 05:18:40 +0300

    Add index.ts file for DevCompanion plugin type definitions

---
+++ src/plugins/devCompanion.dev/types/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated, FieryFlames and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+export * as Recieve from "./recieve";
