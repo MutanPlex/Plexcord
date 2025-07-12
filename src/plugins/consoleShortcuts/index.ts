e6850813a654ab6954a26bfd7852486546af7a08
MutanPlex <barisal54@gmail.com>
2025-07-12 05:17:23 +0300

    Refactor loadLazyChunks to remove dev-only restriction

--- src/plugins/consoleShortcuts/index.ts
+++ src/plugins/consoleShortcuts/index.ts
@@ -105,7 +105,7 @@ function makeShortcuts() {
         wpsearch: search,
         wpex: extract,
         wpexs: (code: string) => extract(findModuleId(code)!),
-        loadLazyChunks: IS_DEV ? loadLazyChunks : () => { throw new Error("loadLazyChunks is dev only."); },
+        loadLazyChunks: loadLazyChunks,
         find,
         findAll: findAll,
         findByProps,
