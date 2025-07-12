936df1ca92ecf0c4d2ca33a8edab6da58416183b
MutanPlex <barisal54@gmail.com>
2025-07-12 05:05:00 +0300

    Add initial type definitions for ignoreTerms plugin in index.d.ts

---
+++ packages/plexcord-types/src/plugins/ignoreTerms/index.d.ts
@@ -0,0 +1,17 @@
+declare const _default: {
+    name: string;
+    description: string;
+    authors: {
+        name: string;
+        id: bigint;
+    }[];
+    patches: {
+        find: string;
+        replacement: {
+            match: RegExp;
+            replace: string;
+        };
+    }[];
+    closeModal(event: any): null;
+} & Record<string, any>;
+export default _default;
