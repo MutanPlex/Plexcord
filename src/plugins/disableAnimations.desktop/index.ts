3cdfe644e72719c48975f302123482d8297c2813
MutanPlex <barisal54@gmail.com>
2025-07-12 05:18:55 +0300

    Add DisableAnimations plugin to disable Discord animations

---
+++ src/plugins/disableAnimations.desktop/index.ts
@@ -0,0 +1,54 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2023 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { PcDevs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { findAll } from "@webpack";
+
+export default definePlugin({
+    name: "DisableAnimations",
+    description: "Disables most of Discord's animations.",
+    authors: [PcDevs.seth],
+    start() {
+        this.springs = findAll(mod => {
+            if (!mod.Globals) return false;
+            return true;
+        });
+
+        for (const spring of this.springs) {
+            spring.Globals.assign({
+                skipAnimation: true,
+            });
+        }
+
+        this.css = document.createElement("style");
+        this.css.innerText = "* { transition: none !important; animation: none !important; }";
+
+        document.head.appendChild(this.css);
+    },
+    stop() {
+        for (const spring of this.springs) {
+            spring.Globals.assign({
+                skipAnimation: false,
+            });
+        }
+
+        if (this.css) this.css.remove();
+    }
+});
