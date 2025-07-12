ef5bbd8f50dd277409cccd283ea723d8f6e2fa15
MutanPlex <barisal54@gmail.com>
2025-07-12 05:14:05 +0300

    Add NicknameIconsAPI plugin to support icons in user profiles

---
+++ src/plugins/_api/nicknameIcons.ts
@@ -0,0 +1,24 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2024 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "NicknameIconsAPI",
+    description: "API to add icons to the nickname, in profiles",
+    authors: [Devs.Nuckyz],
+    patches: [
+        {
+            find: "#{intl::USER_PROFILE_LOAD_ERROR}",
+            replacement: {
+                match: /(\.fetchError.+?\?)null/,
+                replace: (_, rest) => `${rest}Plexcord.Api.NicknameIcons._renderIcons(arguments[0])`
+            }
+        }
+    ]
+});
