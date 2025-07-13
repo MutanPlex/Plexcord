d41648d064d4168ebcfccef90c5b314e4dc49b59
MutanPlex <barisal54@gmail.com>
2025-07-13 14:45:51 +0300

    feat: enhance ShowAllMessageButtons plugin with settings for message deletion and pinning

--- src/plugins/showAllMessageButtons/index.ts
+++ src/plugins/showAllMessageButtons/index.ts
@@ -15,23 +15,66 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
-import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import { Devs, PcDevs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+import { findByPropsLazy } from "@webpack";
+import { MessageActions } from "@webpack/common";
+
+const settings = definePluginSettings({
+    noShiftDelete: {
+        type: OptionType.BOOLEAN,
+        description: "Remove requirement to hold shift for deleting a message.",
+        default: true,
+    },
+    noShiftPin: {
+        type: OptionType.BOOLEAN,
+        description: "Remove requirement to hold shift for pinning a message.",
+        default: true,
+    },
+});
+
+const PinActions = findByPropsLazy("pinMessage", "unpinMessage");
 
 export default definePlugin({
     name: "ShowAllMessageButtons",
-    description: "Always show all message buttons no matter if you are holding the shift key or not.",
-    authors: [Devs.Nuckyz],
+    description:
+        "Always show all message buttons no matter if you are holding the shift key or not.",
+    authors: [Devs.Nuckyz, PcDevs.mochienya],
+    settings,
 
     patches: [
         {
             find: "#{intl::MESSAGE_UTILITIES_A11Y_LABEL}",
-            replacement: {
-                match: /isExpanded:\i&&(.+?),/,
-                replace: "isExpanded:$1,"
-            }
-        }
-    ]
+            replacement: [
+                {
+                    match: /isExpanded:\i&&(.+?),/,
+                    replace: "isExpanded:$1,",
+                },
+                {
+                    predicate: () => settings.store.noShiftDelete,
+                    match: /onClick:.{10,20}(?=,dangerous:!0)/,
+                    replace:
+                        "onClick:() => $self.deleteMessage(arguments[0].message)",
+                },
+                {
+                    predicate: () => settings.store.noShiftPin,
+                    match: /onClick:.{10,30}(?=\},"pin")/,
+                    replace:
+                        "onClick:() => $self.toggleMessagePin(arguments[0]),",
+                },
+            ],
+        },
+    ],
+
+    deleteMessage({ channel_id, id }) {
+        MessageActions.deleteMessage(channel_id, id);
+    },
+    toggleMessagePin({ channel, message }) {
+        if (message.pinned) return PinActions.unpinMessage(channel, message.id);
+
+        PinActions.pinMessage(channel, message.id);
+    },
 });
