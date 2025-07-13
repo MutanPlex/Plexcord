afb97d61f066810943d5b860f635c49ab458a657
MutanPlex <barisal54@gmail.com>
2025-07-13 14:45:00 +0300

    fix: reorganize imports and add sorting settings to ServerInfo plugin

--- src/plugins/serverInfo/index.tsx
+++ src/plugins/serverInfo/index.tsx
@@ -6,10 +6,11 @@
  */
 
 import { findGroupChildrenByChildId, NavContextMenuPatchCallback } from "@api/ContextMenu";
-import { Devs } from "@utils/constants";
-import definePlugin from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import { Guild } from "@plexcord/discord-types";
+import { Devs, PcDevs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
 import { Menu } from "@webpack/common";
-import { Guild } from "discord-types/general";
 
 import { openGuildInfoModal } from "./GuildInfoModal";
 
@@ -25,15 +26,37 @@ const Patch: NavContextMenuPatchCallback = (children, { guild }: { guild: Guild;
     );
 };
 
+export const settings = definePluginSettings({
+    sorting: {
+        type: OptionType.SELECT,
+        description: "Username or if applicable Display Name",
+        options: [
+            {
+                label: "Username",
+                value: "username"
+            },
+            {
+                label: "Display Name",
+                value: "displayname",
+                default: true
+            },
+            {
+                label: "Dont Sort",
+                value: "none",
+            }
+        ]
+    }
+});
+
 export default definePlugin({
     name: "ServerInfo",
     description: "Allows you to view info about a server",
-    authors: [Devs.Ven, Devs.Nuckyz],
+    authors: [Devs.Ven, Devs.Nuckyz, PcDevs.Z1xus],
     dependencies: ["DynamicImageModalAPI"],
     tags: ["guild", "info", "ServerProfile"],
-
     contextMenus: {
         "guild-context": Patch,
         "guild-header-popout": Patch
-    }
+    },
+    settings
 });
