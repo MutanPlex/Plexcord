87bc095daa5431b1106011889f0c6fc38d287b7f
MutanPlex <barisal54@gmail.com>
2025-07-13 18:31:11 +0300

    fix: update project name and copyright details in types.ts

--- src/webpack/types.ts
+++ src/webpack/types.ts
@@ -1,6 +1,7 @@
 /*
- * Vencord, a Discord client mod
- * Copyright (c) 2025 Vendicated, Nuckyz and contributors
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2025 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
