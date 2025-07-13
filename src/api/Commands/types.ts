3a98a1dc2b9628a1367f4b4359f8d33af466a5cb
MutanPlex <barisal54@gmail.com>
2025-07-13 14:08:56 +0300

    Refactor import statement for Channel and Guild types to use @plexcord/discord-types

--- src/api/Commands/types.ts
+++ src/api/Commands/types.ts
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { Channel, Guild } from "discord-types/general";
+import { Channel, Guild } from "@plexcord/discord-types";
 import { Promisable } from "type-fest";
 
 export interface CommandContext {
