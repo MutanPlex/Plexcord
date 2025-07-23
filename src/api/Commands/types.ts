3f098eb1312261aedaa132229e27ab9dc7873b1f
MutanPlex <barisal54@gmail.com>
2025-07-24 00:31:39 +0300

    remove unused imports and clean up command-related interfaces

--- src/api/Commands/types.ts
+++ src/api/Commands/types.ts
@@ -5,47 +5,8 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { Channel, Command, Guild } from "@plexcord/discord-types";
-import { ApplicationCommandOptionType } from "@plexcord/discord-types/enums";
+import { Command } from "@plexcord/discord-types";
 export { ApplicationCommandInputType, ApplicationCommandOptionType, ApplicationCommandType } from "@plexcord/discord-types/enums";
-
-export interface CommandContext {
-    channel: Channel;
-    guild?: Guild;
-}
-
-export interface Option {
-    name: string;
-    displayName?: string;
-    type: ApplicationCommandOptionType;
-    description: string;
-    displayDescription?: string;
-    required?: boolean;
-    options?: Option[];
-    choices?: Array<ChoicesOption>;
-}
-
-export interface ChoicesOption {
-    label: string;
-    value: string;
-    name: string;
-    displayName?: string;
-}
-
-export interface CommandReturnValue {
-    content: string;
-    /** TODO: implement */
-    cancel?: boolean;
-}
-
-export interface Argument {
-    type: ApplicationCommandOptionType;
-    name: string;
-    value: string;
-    focused: undefined;
-    options: Argument[];
-}
-
 
 export interface PlexcordCommand extends Command {
     isPlexcordCommand?: boolean;
