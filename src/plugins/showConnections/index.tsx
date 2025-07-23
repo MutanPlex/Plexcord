57cda648d3c1945fbf82ede83422733d2d537aa2
MutanPlex <barisal54@gmail.com>
2025-07-24 00:50:47 +0300

    update import for OpenInAppPlugin in showConnections index

--- src/plugins/showConnections/index.tsx
+++ src/plugins/showConnections/index.tsx
@@ -29,6 +29,7 @@ import { copyWithToast } from "@utils/misc";
 import definePlugin, { OptionType } from "@utils/types";
 import { findByCodeLazy, findByPropsLazy } from "@webpack";
 import { Tooltip, UserProfileStore } from "@webpack/common";
+import OpenInAppPlugin from "plugins/openInApp";
 
 import { VerifiedIcon } from "./VerifiedIcon";
 
@@ -134,9 +135,8 @@ function CompactConnectionComponent({ connection, theme }: { connection: Connect
                         rel="noreferrer"
                         onClick={e => {
                             if (Plexcord.Plugins.isPluginEnabled("OpenInApp")) {
-                                const OpenInApp = Plexcord.Plugins.plugins.OpenInApp as any as typeof import("../openInApp").default;
                                 // handleLink will .preventDefault() if applicable
-                                OpenInApp.handleLink(e.currentTarget, e);
+                                OpenInAppPlugin.handleLink(e.currentTarget, e);
                             }
                         }}
                     >
