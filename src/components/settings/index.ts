87a7eb59254e2bb104f320f4ca62b84dd6d11448
MutanPlex <barisal54@gmail.com>
2025-07-24 00:34:35 +0300

    add index.ts to export components for settings module

---
+++ src/components/settings/index.ts
@@ -0,0 +1,14 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2025 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+export * from "./AddonCard";
+export * from "./DonateButton";
+export * from "./PluginBadge";
+export * from "./QuickAction";
+export * from "./SpecialCard";
+export * from "./Switch";
+export * from "./tabs";
