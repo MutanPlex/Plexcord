e7bfa6d8f3a700da310371e0e466e02c0be457fc
MutanPlex <barisal54@gmail.com>
2025-07-24 00:38:07 +0300

    add OnlineThemesTab component for managing online theme links

---
+++ src/components/settings/tabs/themes/OnlineThemesTab.tsx
@@ -0,0 +1,57 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2025 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { useSettings } from "@api/Settings";
+import { Margins } from "@utils/margins";
+import { classes } from "@utils/misc";
+import { Card, Forms, TextArea, useState } from "@webpack/common";
+
+export function OnlineThemesTab() {
+    const settings = useSettings(["themeLinks"]);
+
+    const [themeText, setThemeText] = useState(settings.themeLinks.join("\n"));
+
+    // When the user leaves the online theme textbox, update the settings
+    function onBlur() {
+        settings.themeLinks = [...new Set(
+            themeText
+                .trim()
+                .split(/\n+/)
+                .map(s => s.trim())
+                .filter(Boolean)
+        )];
+    }
+
+    return (
+        <>
+            <Card className={classes("pc-warning-card", Margins.bottom16)}>
+                <Forms.FormText>
+                    This section is for advanced users. If you are having difficulties using it, use the
+                    Local Themes tab instead.
+                </Forms.FormText>
+            </Card>
+            <Card className="pc-settings-card">
+                <Forms.FormTitle tag="h5">Paste links to css files here</Forms.FormTitle>
+                <Forms.FormText>One link per line</Forms.FormText>
+                <Forms.FormText>You can prefix lines with @light or @dark to toggle them based on your Discord theme</Forms.FormText>
+                <Forms.FormText>Make sure to use direct links to files (raw or github.io)!</Forms.FormText>
+            </Card>
+
+            <Forms.FormSection title="Online Themes" tag="h5">
+                <TextArea
+                    value={themeText}
+                    onChange={setThemeText}
+                    className={"pc-settings-theme-links"}
+                    placeholder="Enter Theme Links..."
+                    spellCheck={false}
+                    onBlur={onBlur}
+                    rows={10}
+                />
+            </Forms.FormSection>
+        </>
+    );
+}
