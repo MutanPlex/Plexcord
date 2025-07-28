bc7eba0fba178f0a415dd54bbced757aff9fa5c2
MutanPlex <barisal54@gmail.com>
2025-07-28 09:33:00 +0300

    WallpaperFree: Fix

--- src/plugins/wallpaperFree/store.ts
+++ src/plugins/wallpaperFree/store.ts
@@ -5,47 +5,44 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import * as DataStore from "@api/DataStore";
-import { Channel } from "@plexcord/discord-types";
+import { Channel, FluxEmitter, FluxStore } from "@plexcord/discord-types";
 import { proxyLazy } from "@utils/lazy";
-import { Flux, FluxDispatcher } from "@webpack/common";
+import { findByPropsLazy } from "@webpack";
+import { FluxDispatcher } from "@webpack/common";
 
-const DB_KEY = "WallpaperFree_data";
+interface IFlux {
+    PersistedStore: typeof FluxStore;
+    Emitter: FluxEmitter;
+}
+const Flux: IFlux = findByPropsLazy("connectStores");
 
 export const WallpaperFreeStore = proxyLazy(() => {
     const wallpaperChannelMap: Map<string, string> = new Map();
     const wallpaperGuildMap: Map<string, string> = new Map();
     let globalDefault: string | undefined;
 
-    class WallpaperFreeStore extends Flux.Store {
-        async initialize() {
-            const data = await DataStore.get(DB_KEY);
-            if (!data) return;
+    class WallpaperFreeStore extends Flux.PersistedStore {
+        static persistKey = "WallpaperFreeStore";
+
+        // @ts-ignore
+        initialize(previous: { guildMap: Map<string, string>, channelMap: Map<string, string>, globalDefault: string; } | undefined) {
+            if (!previous)
+                return;
 
             wallpaperGuildMap.clear();
             wallpaperChannelMap.clear();
-
-            if (data.channelMap) {
-                for (const [channel, url] of data.channelMap) {
-                    wallpaperChannelMap.set(channel, url);
-                }
+            for (const [channel, url] of previous.channelMap) {
+                wallpaperChannelMap.set(channel, url);
             }
 
-            if (data.guildMap) {
-                for (const [guild, url] of data.guildMap) {
-                    wallpaperGuildMap.set(guild, url);
-                }
+            for (const [guild, url] of previous.guildMap) {
+                wallpaperGuildMap.set(guild, url);
             }
-
-            globalDefault = data.globalDefault;
+            globalDefault = previous.globalDefault;
         }
 
-        public async saveState() {
-            await DataStore.set(DB_KEY, {
-                guildMap: Array.from(wallpaperGuildMap),
-                channelMap: Array.from(wallpaperChannelMap),
-                globalDefault
-            });
+        getState() {
+            return { guildMap: Array.from(wallpaperGuildMap), channelMap: Array.from(wallpaperChannelMap), globalDefault };
         }
 
         getUrl(channel: Channel): string | undefined {
@@ -59,27 +56,24 @@ export const WallpaperFreeStore = proxyLazy(() => {
 
     const store = new WallpaperFreeStore(FluxDispatcher, {
         // @ts-ignore
-        async PC_WALLPAPER_FREE_CHANGE({ guildId, channelId, url }: { guildId: string | undefined, channelId: string | undefined, url: string; }) {
+        PC_WALLPAPER_FREE_CHANGE({ guildId, channelId, url }: { guildId: string | undefined, channelId: string | undefined, url: string; }) {
             if (guildId) {
                 wallpaperGuildMap.set(guildId, url);
             } else if (channelId) {
                 wallpaperChannelMap.set(channelId, url);
             }
-            await store.saveState();
             store.emitChange();
         },
 
-        async PC_WALLPAPER_FREE_CHANGE_GLOBAL({ url }: { url: string | undefined; }) {
+        PC_WALLPAPER_FREE_CHANGE_GLOBAL({ url }: { url: string | undefined; }) {
             globalDefault = url;
-            await store.saveState();
             store.emitChange();
         },
 
-        async PC_WALLPAPER_FREE_RESET() {
+        PC_WALLPAPER_FREE_RESET() {
             wallpaperChannelMap.clear();
             wallpaperGuildMap.clear();
             globalDefault = void 0;
-            await DataStore.del(DB_KEY);
             store.emitChange();
         }
     });
