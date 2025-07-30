6398d9daeff11e8615b5dae382e1a315fe9c8369
MutanPlex <barisal54@gmail.com>
2025-07-30 05:59:05 +0300

    Refactor GuildRoleStore: rename getRoles to getRolesSnapshot and add getSortedRoles method

--- packages/discord-types/src/stores/GuildRoleStore.d.ts
+++ packages/discord-types/src/stores/GuildRoleStore.d.ts
@@ -2,6 +2,6 @@ import { FluxStore, Role } from "..";
 
 export class GuildRoleStore extends FluxStore {
     getRole(guildId: string, roleId: string): Role;
-    getRoles(guildId: string): Record<string, Role>;
-    getAllGuildRoles(): Record<string, Record<string, Role>>;
+    getRolesSnapshot(guildId: string): Record<string, Role>;
+    getSortedRoles(guildId: string): Role[];
 }
