5531981af02265f17659f1ec50133d4be14574ef
MutanPlex <barisal54@gmail.com>
2025-09-10 02:39:04 +0300

    Add CircleIcon component and update MemberCount to use it for online and total counts

---
+++ src/plugins/memberCount/CircleIcon.tsx
@@ -0,0 +1,18 @@
+/*
+ * Plexcord, a modification for Discord's desktop app
+ * Copyright (c) 2025 Vendicated and contributors
+ * Copyright (c) 2025 MutanPlex
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+export function CircleIcon({ className }: { className?: string; }) {
+    return (
+        <svg viewBox="0 0 24 24" className={className}>
+            <circle
+                cx="12"
+                cy="12"
+                r="8"
+            />
+        </svg>
+    );
+}
