e93bc833c2cc7fcbd5e88d510a48760a989d669c
MutanPlex <barisal54@gmail.com>
2025-09-10 02:44:51 +0300

    Add CloudUpload type and extend MessageOptions for polls and attachments

--- src/utils/discord.tsx
+++ src/utils/discord.tsx
@@ -18,7 +18,7 @@
 */
 
 import { MessageObject } from "@api/MessageEvents";
-import { Channel, Guild, GuildFeatures, Message, User } from "@plexcord/discord-types";
+import { Channel, CloudUpload, Guild, GuildFeatures, Message, User } from "@plexcord/discord-types";
 import { ChannelActionCreators, ChannelStore, ComponentDispatch, Constants, FluxDispatcher, GuildStore, i18n, IconUtils, InviteActions, MessageActions, RestAPI, SelectedChannelStore, SelectedGuildStore, UserProfileActions, UserProfileStore, UserSettingsActionCreators, UserUtils } from "@webpack/common";
 import { Except } from "type-fest";
 
@@ -118,6 +118,20 @@ interface MessageOptions {
         replied_user: boolean;
     };
     stickerIds: string[];
+    attachmentsToUpload: CloudUpload[];
+    poll: {
+        allow_multiselect: boolean;
+        answers: Array<{
+            poll_media: {
+                text: string;
+                attachment_ids?: unknown;
+                emoji?: { name: string; id?: string; };
+            };
+        }>;
+        duration: number;
+        layout_type: number;
+        question: { text: string; };
+    };
 }
 
 export function sendMessage(
