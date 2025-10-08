8dd3010c125c768d428350c5f3d1ff0f4f2fef94
MutanPlex <barisal54@gmail.com>
2025-10-08 03:39:40 +0300

    feat: add IgnoreTerms plugin with localization support

--- src/plugins/ignoreTerms/index.tsx
+++ src/plugins/ignoreTerms/index.tsx
@@ -5,6 +5,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { t } from "@api/i18n";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
@@ -12,6 +13,11 @@ export default definePlugin({
     name: "IgnoreTerms",
     description: "Ignore Discord's new terms of service",
     authors: [Devs.D3SOX],
+
+    get displayDescription() {
+        return t("plugin.ignoreTerms.description");
+    },
+
     patches: [
         {
             find: "#{intl::NEW_TERMS_TITLE}",
