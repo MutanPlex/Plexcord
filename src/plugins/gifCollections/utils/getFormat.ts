a65c6c53aab552bbb931cb8c2c4b5932b9d870fa
MutanPlex <barisal54@gmail.com>
2025-10-26 02:50:53 +0300

    fix(getFormat): extract Tenor URL check into a separate variable for clarity

--- src/plugins/gifCollections/utils/getFormat.ts
+++ src/plugins/gifCollections/utils/getFormat.ts
@@ -13,5 +13,6 @@ const videoExtensions = ["mp4", "ogg", "webm", "avi", "wmv", "flv", "mov", "mkv"
 
 export function getFormat(url: string) {
     const extension = getUrlExtension(url);
-    return url.startsWith("https://media.tenor") || extension == null || videoExtensions.includes(extension) ? Format.VIDEO : Format.IMAGE;
+    const isTenor = url.startsWith("https://media.tenor.com/") || url.startsWith("https://media.tenor.co/");
+    return isTenor || extension == null || videoExtensions.includes(extension) ? Format.VIDEO : Format.IMAGE;
 }
