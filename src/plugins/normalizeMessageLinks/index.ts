a512f737d1fa735198ccbae660ae2867d99f2f0a
MutanPlex <barisal54@gmail.com>
2025-11-07 23:13:00 +0300

    feat(localization): add localization for NormalizeMessageLinks plugin

--- src/plugins/normalizeMessageLinks/index.ts
+++ src/plugins/normalizeMessageLinks/index.ts
@@ -5,6 +5,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { t } from "@api/i18n";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
@@ -12,6 +13,11 @@ export default definePlugin({
     name: "NormalizeMessageLinks",
     description: "Strip canary/ptb from message links",
     authors: [Devs.bb010g],
+
+    get displayDescription() {
+        return t("plugin.normalizeMessageLinks.description");
+    },
+
     patches: [
         {
             find: "#{intl::COPY_MESSAGE_LINK}",
