3a5352261c857684fd821665b54b9117c6e32110
MutanPlex <barisal54@gmail.com>
2025-11-08 04:25:34 +0300

    feat(overrideForumDefaults): add localization for overrideForumDefaults plugin options

--- src/plugins/overrideForumDefaults/index.tsx
+++ src/plugins/overrideForumDefaults/index.tsx
@@ -5,26 +5,41 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { t } from "@api/i18n";
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 
 const settings = definePluginSettings({
     defaultLayout: {
+        get label() {
+            return t("plugin.overrideForumDefaults.option.defaultLayout.label");
+        },
+        get description() {
+            return t("plugin.overrideForumDefaults.option.defaultLayout.description");
+        },
         type: OptionType.SELECT,
-        options: [
-            { label: "List", value: 1, default: true },
-            { label: "Gallery", value: 2 }
-        ],
-        description: "Which layout to use as default"
+        get options() {
+            return [
+                { label: t("plugin.overrideForumDefaults.option.defaultLayout.list"), value: 1, default: true },
+                { label: t("plugin.overrideForumDefaults.option.defaultLayout.gallery"), value: 2 }
+            ];
+        }
     },
     defaultSortOrder: {
+        get label() {
+            return t("plugin.overrideForumDefaults.option.defaultSortOrder.label");
+        },
+        get description() {
+            return t("plugin.overrideForumDefaults.option.defaultSortOrder.description");
+        },
         type: OptionType.SELECT,
-        options: [
-            { label: "Recently Active", value: 0, default: true },
-            { label: "Date Posted", value: 1 }
-        ],
-        description: "Which sort order to use as default"
+        get options() {
+            return [
+                { label: t("plugin.overrideForumDefaults.option.defaultSortOrder.recentlyActive"), value: 0, default: true },
+                { label: t("plugin.overrideForumDefaults.option.defaultSortOrder.datePosted"), value: 1 }
+            ];
+        }
     }
 });
 
@@ -32,6 +47,11 @@ export default definePlugin({
     name: "OverrideForumDefaults",
     description: "Allows you to override default forum layout/sort order. you can still change it on a per-channel basis",
     authors: [Devs.Inbestigator],
+
+    get displayDescription() {
+        return t("plugin.overrideForumDefaults.description");
+    },
+
     patches: [
         {
             find: "getDefaultLayout(){",
