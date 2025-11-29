2a3a9e1971c7ecae0a229a2f0eb9378a4cfe8599
MutanPlex <barisal54@gmail.com>
2025-11-29 17:55:24 +0300

    refactor: add stickerPaste plugin with customizable options and internationalization support

--- src/plugins/stickerPaste/index.ts
+++ src/plugins/stickerPaste/index.ts
@@ -5,6 +5,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import { t } from "@api/i18n";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
@@ -12,6 +13,10 @@ export default definePlugin({
     name: "StickerPaste",
     description: "Makes picking a sticker in the sticker picker insert it into the chatbox instead of instantly sending",
     authors: [Devs.ImBanana],
+
+    get displayDescription() {
+        return t("plugin.stickerPaste.description");
+    },
 
     patches: [
         {
